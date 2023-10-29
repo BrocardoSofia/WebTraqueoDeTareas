931a64ec18d1e17cbe177290e522d617eacaa786
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-10-29 17:41:49 -0300

    add component inicio

--- tasky/src/app/components/home/components/main/main.module.ts
+++ tasky/src/app/components/home/components/main/main.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent
+  ],
   imports: [
     CommonModule
   ]

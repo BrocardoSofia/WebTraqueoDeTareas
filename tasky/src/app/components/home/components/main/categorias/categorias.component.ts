35e58c8dcc616c92d0eb84ddf91fe67b167df12f
Vanina Pintos <soyvaninapintoss@gmail.com>
2023-10-30 19:03:04 -0300

    update
    html basico y logica de categorias terminado

--- tasky/src/app/components/home/components/main/categorias/categorias.component.ts
+++ tasky/src/app/components/home/components/main/categorias/categorias.component.ts
@@ -10,14 +10,15 @@ import { CategoriasService } from 'src/app/services/categorias.service';
 export class CategoriasComponent {
 
   catObj : Categoria = new Categoria();
-
   catArr : Categoria[] = [];
-
   newCat : string = '';
+  editar : string = '';
 
   constructor(private categoriasService : CategoriasService){}
 
   ngOnInit() : void{
+    this.editar = '',
+    this.newCat = '',
     this.catObj = new Categoria();
     this.catArr = [];
     this.getCategorias();
@@ -31,7 +32,7 @@ export class CategoriasComponent {
     })
   }
 
-  agregarCat(){
+  agregarCategoria(){
     this.catObj.nombre = this.newCat;
     this.categoriasService.agregarCategoria(this.catObj).subscribe(res =>{
       this.ngOnInit();
@@ -42,6 +43,7 @@ export class CategoriasComponent {
   }
 
   editarCategoria(){
+    this.catObj.nombre = this.editar;
     this.categoriasService.editarCategoria(this.catObj).subscribe(res => {
       this.ngOnInit();
     }, e => {
@@ -51,9 +53,14 @@ export class CategoriasComponent {
 
   borrarCategoria(c : Categoria){
     this.categoriasService.borrarCategoria(c).subscribe(res =>  {
-
+        this.ngOnInit();
     }, e => {
       alert("Ha ocurrido un error al intentar borrar la categoria");
-    })
+    });
+  }
+
+  call(c : Categoria){
+    this.catObj = c;
+    this.editar = c.nombre;
   }
 }

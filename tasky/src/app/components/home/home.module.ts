aa0ab8b26ea086fe04d27792841b14d0a4faf3be
Vanina Pintos <soyvaninapintoss@gmail.com>
2023-10-29 22:41:16 -0300

    update
    correcion de error de componente header no reconocido por el componente home

--- tasky/src/app/components/home/home.module.ts
+++ tasky/src/app/components/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
-
 
 @NgModule({
   declarations: [
+    HomeComponent,
     MainComponent,
     HeaderComponent
   ],

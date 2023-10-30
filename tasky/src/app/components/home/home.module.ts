70449c1b4522cfae5310038b304b117670561bad
Vanina Pintos <soyvaninapintoss@gmail.com>
2023-10-30 01:29:42 -0300

    update
    - componentes header, aside y footer dentro del modulo home.
    - eliminacion de componente main para crear un modulo main

--- tasky/src/app/components/home/home.module.ts
+++ tasky/src/app/components/home/home.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
-import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AsideComponent } from './components/aside/aside.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    MainComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent
   ],
   imports: [
     CommonModule,

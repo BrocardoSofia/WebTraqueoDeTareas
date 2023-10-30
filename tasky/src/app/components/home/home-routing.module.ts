99d48d8e5294a4210cd2c36a2e4b4035c9331299
Vanina Pintos <soyvaninapintoss@gmail.com>
2023-10-30 11:22:27 -0300

    FUNCIONA

--- tasky/src/app/components/home/home-routing.module.ts
+++ tasky/src/app/components/home/home-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { InicioComponent } from './components/main/inicio/inicio.component';
+import { CategoriasComponent } from './components/main/categorias/categorias.component';
 
 const routes: Routes = [
   {
     path : '',
-    component : HomeComponent
-  }
+    component : InicioComponent
+  },
+  {
+    path : 'categorias',
+    component : CategoriasComponent
+  },
 ];
 
 @NgModule({
--- tasky/src/app/components/home/home.module.ts
+++ tasky/src/app/components/home/home.module.ts
@@ -6,15 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AsideComponent } from './components/aside/aside.component';
 import { ButtonModule } from 'primeng/button';
-import { MainComponent } from './components/main/main.component';
 import { InicioComponent } from './components/main/inicio/inicio.component';
-import { CategoriasComponent } from './components/main/categorias/categorias.component';
-import { TareasComponent } from './components/main/tareas/tareas.component';
-import { ConfAguaComponent } from './components/main/conf-agua/conf-agua.component';
-import { ConfEjercicioComponent } from './components/main/conf-ejercicio/conf-ejercicio.component';
-import { ConfPomodoroComponent } from './components/main/conf-pomodoro/conf-pomodoro.component';
-import { UsuarioComponent } from './components/main/usuario/usuario.component';
-import { EstadisticasComponent } from './components/main/estadisticas/estadisticas.component';
 
 @NgModule({
   declarations: [
@@ -22,15 +14,7 @@ import { EstadisticasComponent } from './components/main/estadisticas/estadistic
     HeaderComponent,
     FooterComponent,
     AsideComponent,
-    MainComponent,
     InicioComponent,
-    CategoriasComponent,
-    TareasComponent,
-    ConfAguaComponent,
-    ConfEjercicioComponent,
-    ConfPomodoroComponent,
-    UsuarioComponent,
-    EstadisticasComponent
   ],
   imports: [
     CommonModule,

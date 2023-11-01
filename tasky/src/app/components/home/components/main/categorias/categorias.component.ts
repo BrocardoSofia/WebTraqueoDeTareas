6e9a523cd0e1ef14b62ac4f0ba9b53ff2d4ab327
Vanina Pintos <soyvaninapintoss@gmail.com>
2023-11-01 11:33:33 -0300

    update
    cambio de puerto de json server
    validacion de categorias

--- tasky/src/app/components/home/components/main/categorias/categorias.component.ts
+++ tasky/src/app/components/home/components/main/categorias/categorias.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { Categoria } from './Categoria';
 import { CategoriasService } from 'src/app/services/categorias.service';
-import { FormGroup,FormControl,Validators, ValidatorFn } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn,
+} from '@angular/forms';
 import { AbstractControl } from '@angular/forms';
 
 @Component({
@@ -10,85 +15,99 @@ import { AbstractControl } from '@angular/forms';
   styleUrls: ['./categorias.component.css'],
 })
 export class CategoriasComponent {
-
-  //VALIDACION AL AGREGAR UNA NUEVA CATEGORIA
+  //VALIDACION REACTIVA AL AGREGAR UNA NUEVA CATEGORIA
   nuevaCategoriaForm = new FormGroup({
-    nombre : new FormControl('',[Validators.maxLength(20)])
-  })
+    nombre: new FormControl('', [Validators.maxLength(20)]),
+  });
 
-  get nombre(){
-    return this.nuevaCategoriaForm.get('nombre')
+  get nombre() {
+    return this.nuevaCategoriaForm.get('nombre');
   }
 
   //CRUD CATEGORIAS
-  catObj : Categoria = new Categoria();
-  catArr : Categoria[] = [];
-  newCat : string = '';
-  editar : string = '';
+  catObj: Categoria = new Categoria();
+  catArr: Categoria[] = [];
+  newCat: string = '';
+  editar: string = '';
 
-  constructor(private categoriasService : CategoriasService){}
+  constructor(private categoriasService: CategoriasService) {}
 
-  ngOnInit() : void{
-    this.editar = '',
-    this.newCat = '',
-    this.catObj = new Categoria();
+  ngOnInit(): void {
+    (this.editar = ''), (this.newCat = ''), (this.catObj = new Categoria());
     this.catArr = [];
     this.getCategorias();
   }
 
-  getCategorias(){
-    this.categoriasService.getCategorias().subscribe(res => {
-      this.catArr = res;
-    }, e => {
-      alert("No es posible cargar las categorias");
-    })
+  getCategorias() {
+    this.categoriasService.getCategorias().subscribe(
+      (res) => {
+        this.catArr = res;
+      },
+      (e) => {
+        alert('No es posible cargar las categorias');
+      }
+    );
   }
 
-  agregarCategoria(){
-
-    let flag = 0;
-
-    //VALIDACION DE QUE NO EXISTA LA CATEGORIA
-    //habria que validar si esta vacio?
-    this.catArr.forEach( c => {
-      if(c.nombre.toUpperCase() == this.newCat.toUpperCase()){
-          flag = 1;
-      }
-    } )
-
-    if(!flag){
-      this.catObj.nombre = this.newCat;
-
-      this.categoriasService.agregarCategoria(this.catObj).subscribe(res =>{
-        this.ngOnInit();
-        this.newCat = '';
-      },e => {
-        alert(e);
-      })
-    }else {
+  agregarCategoria() {
+    if (this.existe(this.newCat)) {
       alert('Esa categoria ya existe');
+    } else {
+      this.catObj.nombre = this.newCat;
+      this.categoriasService.agregarCategoria(this.catObj).subscribe(
+        (res) => {
+          this.ngOnInit();
+          this.newCat = '';
+        },
+        (e) => {
+          alert(e);
+        }
+      );
     }
   }
 
-  editarCategoria(){
-    this.catObj.nombre = this.editar;
-    this.categoriasService.editarCategoria(this.catObj).subscribe(res => {
-      this.ngOnInit();
-    }, e => {
-      alert("Ha ocurrido un error al intentar editar la categoria");
-    })
+  editarCategoria() {
+    if (this.existe(this.editar)) {
+      alert('Esa catgoria ya existe');
+    } else {
+      this.catObj.nombre = this.editar;
+      this.categoriasService.editarCategoria(this.catObj).subscribe(
+        (res) => {
+          this.ngOnInit();
+        },
+        (e) => {
+          alert('Ha ocurrido un error al intentar editar la categoria');
+        }
+      );
+    }
   }
 
-  borrarCategoria(c : Categoria){
-    this.categoriasService.borrarCategoria(c).subscribe(res =>  {
+  borrarCategoria(c: Categoria) {
+    this.categoriasService.borrarCategoria(c).subscribe(
+      (res) => {
         this.ngOnInit();
-    }, e => {
-      alert("Ha ocurrido un error al intentar borrar la categoria");
-    });
+      },
+      (e) => {
+        alert('Ha ocurrido un error al intentar borrar la categoria');
+      }
+    );
   }
 
-  call(c : Categoria){
+  call(c: Categoria) {
     this.catObj = c;
     this.editar = c.nombre;
+  }
+
+  //VALIDACION DE QUE NO EXISTA LA CATEGORIA
+  existe(nombreCat: string): boolean {
+    let flag: boolean = false;
+
+    this.catArr.forEach((c) => {
+      if (c.nombre.toUpperCase() == nombreCat.toUpperCase()) {
+        flag = true;
+      }
+    });
+
+    return flag;
   }
 }

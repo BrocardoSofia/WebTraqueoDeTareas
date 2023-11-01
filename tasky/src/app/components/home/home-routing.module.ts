021270490f07e9a4f213af63f62b629a5a4c143a
Vanina Pintos <soyvaninapintoss@gmail.com>
2023-11-01 20:34:02 -0300

    update validaciones
    validaciones finalizado

--- tasky/src/app/components/home/home-routing.module.ts
+++ tasky/src/app/components/home/home-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/main/inicio/inicio.component';
 import { CategoriasComponent } from './components/main/categorias/categorias.component';
+import { TareasComponent } from './components/main/tareas/tareas.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path : 'categorias',
     component : CategoriasComponent
+  },
+  {
+    path : 'tareas',
+    component : TareasComponent
   },
 ];
 

f01065df5c589fefb9fa86f51b3982861919584a
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-11-01 17:30:31 -0300

    update
    cambio de ubicación de los componentes en home y creación de componente body

--- tasky/src/app/components/home/home-routing.module.ts
+++ tasky/src/app/components/home/home-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { ConfAguaComponent } from './components/conf-agua/conf-agua.component';
+import { ConfEjercicioComponent } from './components/conf-ejercicio/conf-ejercicio.component';
+import { ConfPomodoroComponent } from './components/conf-pomodoro/conf-pomodoro.component';
 
 const routes: Routes = [
-  {
-    path : '',
-    component : HomeComponent
-  }
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'categorias', component: CategoriasComponent},
+  {path: 'usuario', component: UsuarioComponent},
+  {path: 'conf-agua', component: ConfAguaComponent},
+  {path: 'conf-ejercicio', component: ConfEjercicioComponent},
+  {path: 'conf-pomodoro', component: ConfPomodoroComponent}
 ];
 
 @NgModule({

72dcdcd4397a2dd26959fd61a8d5b08f97d1a922
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-11-02 13:17:03 -0300

    componente configuracion
    update

--- tasky/src/app/components/home/home-routing.module.ts
+++ tasky/src/app/components/home/home-routing.module.ts
@@ -4,9 +4,7 @@ import { HomeComponent } from './home.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { CategoriasComponent } from './components/categorias/categorias.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { ConfAguaComponent } from './components/conf-agua/conf-agua.component';
-import { ConfEjercicioComponent } from './components/conf-ejercicio/conf-ejercicio.component';
-import { ConfPomodoroComponent } from './components/conf-pomodoro/conf-pomodoro.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
 import { EstadisticasComponent } from './components/estadisticas/estadisticas.component';
 
 const routes: Routes = [
@@ -14,9 +12,7 @@ const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
   {path: 'categorias', component: CategoriasComponent},
   {path: 'usuario', component: UsuarioComponent},
-  {path: 'conf-agua', component: ConfAguaComponent},
-  {path: 'conf-ejercicio', component: ConfEjercicioComponent},
-  {path: 'conf-pomodoro', component: ConfPomodoroComponent},
+  {path: 'configuracion', component: ConfiguracionComponent},
   {path: 'estadisticas', component: EstadisticasComponent},
 ];
 

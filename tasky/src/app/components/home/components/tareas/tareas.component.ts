4da8968fdea5381fcb502ebb796e80e51d30d8c2
Vanina Pintos <soyvaninapintoss@gmail.com>
2023-11-07 12:18:17 -0300

    update

--- tasky/src/app/components/home/components/tareas/tareas.component.ts
+++ tasky/src/app/components/home/components/tareas/tareas.component.ts
@@ -10,13 +10,25 @@ import { Message } from 'primeng/api';
 })
 export class TareasComponent {
 
-  arrTareas: Tarea[] = []
-  nuevaTarea: string = ""
-  tareaObj: Tarea = new Tarea()
+  arrTareas: Tarea[] = [];
+  arrNombres: string[] = [];
+  nuevaTarea: string = "";
+  tareaObj: Tarea = new Tarea();
+  tareaSeleccionada: string = ''
+
+  startTimer: any;
+  corriendo: boolean = false;
+  pausado: boolean = false;
+  terminado: boolean = false;
+  hr: any = '0' + 0;
+  min: any = '0' + 0;
+  seg: any = '0' + 0;
+  ms: any = '0' + 0;
 
   advertencia: Message[] = [];
   advertir: boolean = false;
   mensaje: string = 'ha ocurrido un error inesperado'
+
 
   constructor(private tareasService: TareasService) { }
 
@@ -24,6 +36,10 @@ export class TareasComponent {
     this.tareasService.getTareas().subscribe(
       (res) => {
         this.arrTareas = res;
+        res.forEach(r => {
+          this.arrNombres.push(r.nombre)
+        })
+        this.limpiarArreglo();
       },
       (e) => {
         alert('error al cargar las tareas');
@@ -31,37 +47,34 @@ export class TareasComponent {
     )
   }
 
-  agregarTarea() {
-
-    if (this.existe(this.nuevaTarea)) {
-      this.updateMensaje('Esa tarea ya existe');
-      this.advertir = true;
-    } else if (this.vacio(this.nuevaTarea) || this.nuevaTarea.length == 0) {
-      this.updateMensaje('Nombre de tarea invalida');
-      this.advertir = true;
-    } else {
-      this.tareaObj.nombre = this.nuevaTarea;
-      this.tareasService.agregarTarea(this.tareaObj).subscribe(
-        (res) => {
-          this.ngOnInit();
-          this.nuevaTarea = '';
-        },
-        (e) => {
-          alert('error al intentar agregar una tarea');
-        }
-      )
-    }
+  limpiarArreglo() {
+    this.arrNombres = this.arrNombres.filter(function (elem, index, self) {
+      return index === self.indexOf(elem);
+    })
   }
 
-  refrescarTabla(){
-    let cargar = '';
+  // agregarTarea() {
 
-    for (let t of this.arrTareas){
-      cargar+= `<tr><td>${t.nombre}</td><td>${t.tiempo}</td><td>${t.fecha}</td></tr>`
-    }
-
-    document.getElementById("tabla-tareas").innerHTML = cargar;
-  }
+  //   if (this.existe(this.nuevaTarea)) {
+  //     this.updateMensaje('Esa tarea ya existe');
+  //     this.advertir = true;
+  //   } else if (this.vacio(this.nuevaTarea) || this.nuevaTarea.length == 0) {
+  //     this.updateMensaje('Nombre de tarea invalida');
+  //     this.advertir = true;
+  //   } else {
+  //     this.tareaObj.nombre = this.nuevaTarea;
+  //     this.tareasService.agregarTarea(this.tareaObj).subscribe(
+  //       (res) => {
+  //         // this.borrar()
+  //         this.ngOnInit();
+  //         this.nuevaTarea = '';
+  //       },
+  //       (e) => {
+  //         alert('error al intentar agregar una tarea');
+  //       }
+  //     )
+  //   }
+  // }
 
   existe(nombreTarea: string): boolean {
     let flag = false;
@@ -94,14 +107,67 @@ export class TareasComponent {
     ];
   }
 
+
+  start() {
+    if (!this.corriendo) {
+      this.corriendo = true;
+      this.startTimer = setInterval(() => {
+
+        if (!this.pausado) {
+          this.ms++;
+          this.ms = this.ms < 10 ? '0' + this.ms : this.ms;
+
+          if (this.ms === 100) {
+            this.seg++;
+            this.seg = this.seg < 10 ? '0' + this.seg : this.seg;
+            this.ms = '0' + 0;
+          }
+
+          if (this.seg === 60) {
+            this.min++;
+            this.min = this.min < 10 ? '0' + this.min : this.min;
+            this.seg = '0' + 0;
+          }
+
+          if (this.min === 60) {
+            this.hr++;
+            this.hr = this.hr < 10 ? '0' + this.hr : this.hr;
+            this.min = '0' + 0;
+          }
+        }
+      }, 10)
+    }
+  }
+
+  reanudar() {
+    this.pausado = false;
+  }
+
+  pause() {
+    this.pausado = true;
+  }
+
+  stop() {
+    clearInterval(this.startTimer)
+    this.corriendo = false;
+    this.ngOnInit()
+  }
+
   ngOnInit(): void {
 
     this.arrTareas = [];
-    this.nuevaTarea = "";
+    this.arrNombres = [];
     this.tareaObj = new Tarea();
+    this.nuevaTarea = "";
+    this.advertir = false;
+    this.corriendo = false;
+
+    this.hr = '0' + 0;
+    this.min = '0' + 0;
+    this.seg = '0' + 0;
+    this.ms = '0' + 0;
 
     this.getTareas();
     this.cargarMensaje();
-    this.refrescarTabla()
   }
 }

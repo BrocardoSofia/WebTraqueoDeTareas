7ddbf6a7b0dd2d4eef21dcdb496ba2e6b4988a83
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-11-20 01:06:20 -0300

    update

--- tasky/src/app/components/home/components/inicio/inicio.component.ts
+++ tasky/src/app/components/home/components/inicio/inicio.component.ts
@@ -45,7 +45,7 @@ function pedirAPI() {
     let lon: string = '-57.5528';
     let API_key: string = '';
 
-    xhr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + API_key + '&lang=es');
+    xhr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + API_key + '&units=metric&lang=es');
     xhr.responseType = 'json';
 
     xhr.onload = function () {
@@ -64,35 +64,122 @@ function pedirAPI() {
 
 async function llamadaAPI() {
   try {
-    await pedirAPI();
-    cargarClima();
+    let loc = '';
+    if(loc === '')
+    {
+      await pedirAPI();
+      cargarClima();
+    }
+    else
+    {
+      cargarNoClima();
+    }
   }
   catch (error) {
     console.log(error);
-    let loc = false;
-    if (!loc)
-    {
-      //let weather = document.getElementById("weather");
-      //let imagen = document.createElement("img");
-      //imagen.src = "./assets/weather/clima.png";
-      //imagen.width = 250;
-      //weather?.appendChild(imagen);
-    }
   }
 
 }
 
+function cargarNoClima() {
+  let weather = document.getElementById("iconoClima");
+  let infoWeather = document.getElementById("infoClima");
+  let imagen = document.createElement("img");
+  let info = document.createElement("section");
+  let texto1 = document.createTextNode("Agregar localizacion ");
+  let texto2 = document.createTextNode("para obtener datos ");
+  let texto3 = document.createTextNode("del clima");
+  let salto;
+
+  imagen.src = "./assets/weather/clima.png";
+  imagen.width = 300;
+
+  info.style.color = "#151E59";
+  info.style.fontSize = "30px";
+  info.style.fontFamily = "Poppins";
+  info.style.marginBottom = "150px";
+
+  weather?.appendChild(imagen);
+  info.appendChild(texto1);
+  salto = document.createElement("br");
+  info.appendChild(salto);
+  info.appendChild(texto2);
+  salto = document.createElement("br");
+  info.appendChild(salto);
+  info.appendChild(texto3);
+  infoWeather?.appendChild(info);
+}
+
 function cargarClima() {
-  let localizacion = false;
-  console.log(jsonData);
-  console.log('clima: ' + jsonData.name);
-  let weather = document.getElementById("weather");
+  let info = document.getElementById("infoClima");
+  let weather = document.getElementById("iconoClima");
+
   let imagen = document.createElement("img");
 
-  if (jsonData.weather)
-  {
+  let infoSection = document.createElement("section");
+  infoSection.style.display = "flex";
+  infoSection.style.flexDirection = "column";
+  infoSection.style.alignContent = "space-between";
+
+  let infoClimaSection = document.createElement("section");
+  infoClimaSection.style.display = "flex";
+  infoClimaSection.style.alignContent = "flex-start";
+
+  let infoLocalizacion = document.createElement("section");
+
+  let tempSection = document.createElement("section");
+  let espSection = document.createElement("section");
+
+  let pTemp = document.createElement("p");
+  let tTemp = document.createTextNode(jsonData.main.temp + "ºC");
+  let pDesc = document.createElement("p");
+  let tDesc = document.createTextNode("");
+
+  let pLoc = document.createElement("p");
+  let tLoc = document.createTextNode(jsonData.name);
+
+  pTemp.style.marginBottom = "0px";
+
+  pLoc.appendChild(tLoc);
+  pLoc.style.fontSize = "18px";
+  pLoc.style.fontFamily = "Poppins, sans-serif";
+  pLoc.style.color = "#151E59";
+  pLoc.style.fontWeight = "bold";
+  infoLocalizacion.appendChild(pLoc);
+
+
+  if (jsonData.weather) {
     imagen.src = "./assets/weather/" + jsonData.weather[0].icon + ".png";
-    imagen.width = 200;
+    imagen.width = 300;
     weather?.appendChild(imagen);
+
+    tDesc = document.createTextNode(jsonData.weather[0].description);
   }
+
+  pTemp.appendChild(tTemp);
+  pDesc.appendChild(tDesc);
+  pDesc.style.textTransform = "capitalize";
+
+  pTemp.style.fontSize = "60px";
+  pTemp.style.fontFamily = "Poppins, sans-serif";
+  pTemp.style.color = "#151E59";
+  pTemp.style.fontWeight = "bold";
+
+  pDesc.style.fontSize = "20px";
+  pDesc.style.fontFamily = "Poppins, sans-serif";
+  pDesc.style.color = "#151E59";
+  pDesc.style.fontWeight = "bold";
+
+  tempSection.appendChild(pTemp);
+  tempSection.appendChild(pDesc);
+
+  espSection.style.margin = "2px";
+  espSection.style.borderLeft = "thick solid #151E59";
+
+  infoSection.appendChild(tempSection);
+  infoSection.style.marginBottom ="120px";
+
+  info?.appendChild(infoSection);
+  info?.appendChild(infoLocalizacion);
+
 }

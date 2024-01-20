8783e5805f39d20e0f5d7b11030278108cef50ab
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-01-20 15:55:18 -0300

    update server

--- server/dist/db/connection.js
+++ server/dist/db/connection.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
+const config = require('./config.js');
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new sequelize_1.Sequelize('b5fmec0udyndxf0l9sld', 'upm83ojhtpk23o1g', 'W7LK7WcFsIol6cxn05Ln', {
-    host: 'b5fmec0udyndxf0l9sld-mysql.services.clever-cloud.com',
+const sequelize = new sequelize_1.Sequelize(config.sequelize1, config.sequelize2, config.sequelize3, {
+    host: config.host,
     dialect: 'mysql'
 });
 exports.default = sequelize;
